02dbe01aa01f397ae8150822eb15c413ba7a9a5a
Wilson Luiz Prosdocimo <wilson@wprosdocimo.com.br>
2020-05-28 07:49:56 -0300

    Finalizando trabalhos do dia:
    - Adicionado valores default para iniciar calculos
    - Alterado valor inicial da quantidade de bordados para 1 de forma a não quebrar se o usuário não adicionar nada.
    - Removido array de strings do arquivo values/strings.xml

--- Bordados/app/src/main/java/br/com/wprosdocimo/bordados/ui/activifty/Inicial.kt
+++ Bordados/app/src/main/java/br/com/wprosdocimo/bordados/ui/activifty/Inicial.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.ListAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import br.com.wprosdocimo.bordados.R
@@ -22,27 +21,22 @@ class Inicial : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         Bastidor("B", 140, 200),
         Bastidor("C", 50, 50)
     )
+    val consumo_linha_bordado_por_1000_pontos = 6.5
+    val consumo_linha_bobina_por_1000_pontos = 2.5
+    val custo_cone_linha_bordado = 10.0
+    val custo_cone_linha_bobina = 30.0
+    val quantidade_linha_bordado_por_cone = 4000
+    val quantidade_linha_bobina_por_cone = 15000
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.inicial_activity)
 
-        ArrayAdapter(this, android.R.layout.simple_spinner_item, bastidores).also {adapter ->
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            bastidor_spinner.adapter = adapter
-        }
+        configuraSpinner()
+        configuraBotaoCalcular()
+    }
 
-//        ArrayAdapter.createFromResource(this,
-//            bastidores,
-//            android.R.layout.simple_spinner_item).also { adapter ->
-//            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-//            bastidor_spinner.adapter = adapter
-//        }
-
-        bastidor_spinner.onItemSelectedListener = this
-
-        calcula_button.setOnClickListener{
+    private fun configuraBotaoCalcular() {
+        calcula_button.setOnClickListener {
             val pontos = pontos_editText.text.toString()
             val cores = cores_editText.text.toString()
             val quantidade = qtde_editText.text.toString()
@@ -51,15 +45,37 @@ class Inicial : AppCompatActivity(), AdapterView.OnItemSelectedListener {
                 cores = cores.toInt(),
                 bastidor = bastidor
             )
-            Toast.makeText(this,
+
+            val custo_metro_linha_bordado = custo_cone_linha_bordado / quantidade_linha_bordado_por_cone
+            val consumo_do_bordado = ( bordado.pontos / 1000 ) * consumo_linha_bordado_por_1000_pontos
+            val custo_metro_linha_bobina = custo_cone_linha_bobina / quantidade_linha_bobina_por_cone
+            val consumo_do_bobina = ( bordado.pontos / 1000 ) * consumo_linha_bobina_por_1000_pontos
+            val custo_linha_bobina = custo_metro_linha_bobina * consumo_do_bobina
+            val custo_linha_bordado = custo_metro_linha_bordado * consumo_do_bordado
+            val custo = ( custo_linha_bordado + custo_linha_bobina ) * quantidade.toInt()
+
+            Toast.makeText(
+                this,
                 "Pontos: ${bordado.pontos}," +
                         " Cores: ${bordado.cores}," +
                         " Qtde: $quantidade," +
-                        " Bastidor (bordado): ${bordado.bastidor}",
-                Toast.LENGTH_LONG)
+                        " Bastidor (bordado): ${bordado.bastidor}" +
+                        "Custo Calculado: $custo",
+                Toast.LENGTH_LONG
+            )
                 .show()
         }
+    }
 
+    private fun configuraSpinner() {
+        setContentView(R.layout.inicial_activity)
+
+        ArrayAdapter(this, android.R.layout.simple_spinner_item, bastidores).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            bastidor_spinner.adapter = adapter
+        }
+
+        bastidor_spinner.onItemSelectedListener = this
     }
 
     override fun onNothingSelected(parent: AdapterView<*>?) {
