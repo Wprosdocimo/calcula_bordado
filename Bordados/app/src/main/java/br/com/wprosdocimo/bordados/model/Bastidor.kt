c43fa09b0e6b391fb8c76025eceba3eceb67f9f2
Wilson Luiz Prosdocimo <wilson@wprosdocimo.com.br>
2020-05-28 07:03:20 -0300

    Adicionado valores padão para as properties e adicionado metodo toString para retornar no formato necessário para spinner.

--- Bordados/app/src/main/java/br/com/wprosdocimo/bordados/model/Bastidor.kt
+++ Bordados/app/src/main/java/br/com/wprosdocimo/bordados/model/Bastidor.kt
@@ -1,8 +1,14 @@
 package br.com.wprosdocimo.bordados.model
 
 class Bastidor(
-    val nome: String,
-    val altura: Int,
-    val largura: Int
+    val nome: String = "",
+    val altura: Int = 0,
+    val largura: Int = 0
 ) {
+    override fun toString(): String {
+        if ( nome == "" ) {
+            return nome
+        }
+        return "$nome ( $altura x $largura mm)"
+    }
 }
