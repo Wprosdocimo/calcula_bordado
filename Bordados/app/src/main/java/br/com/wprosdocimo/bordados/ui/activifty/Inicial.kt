cd40674ac86d854b33d30188238bf53ee7c71caf
Wilson Luiz Prosdocimo <wilson@wprosdocimo.com.br>
2020-06-05 08:06:57 -0300

    Finalizando trabalhos do dia 05/06/2020:
    - Adicionado calculo de mão de obra e custos fixos
    - Separado funções de calculo
    - Alterado funções para retornar valores em BigDecimal
    - Criado função para formatar BigDecimal.

--- Bordados/app/src/main/java/br/com/wprosdocimo/bordados/ui/activifty/Inicial.kt
+++ Bordados/app/src/main/java/br/com/wprosdocimo/bordados/ui/activifty/Inicial.kt
@@ -1,5 +1,7 @@
 package br.com.wprosdocimo.bordados.ui.activifty
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
@@ -7,11 +9,14 @@ import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import br.com.wprosdocimo.bordados.R
+import br.com.wprosdocimo.bordados.extension.formataParaBrasileiro
 import br.com.wprosdocimo.bordados.model.Bastidor
 import br.com.wprosdocimo.bordados.model.Bordado
 import kotlinx.android.synthetic.main.inicial_activity.*
+import java.math.BigDecimal
 
-class Inicial : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+//class Inicial : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+class Inicial : AppCompatActivity() {
     var bastidor: Bastidor = Bastidor()
     // bastidores originais JANOME
     // A (110 x 125 mm)  B (140 x 200 mm) C (50 x 50 mm)
@@ -33,46 +38,102 @@ class Inicial : AppCompatActivity(), AdapterView.OnItemSelectedListener {
             val pontos = pontos_editText.text.toString()
             val cores = cores_editText.text.toString()
             val quantidade = qtde_editText.text.toString()
+            val bastidorSelecionado: Bastidor = bastidor_spinner.selectedItem as Bastidor
 
             val bordado = Bordado(
                 pontos = pontos.toInt(),
                 cores = cores.toInt(),
-                bastidor = bastidor
+                bastidor = bastidorSelecionado
             )
 
-            val custo = calcula_custo_total(
+            val custo = calcula_custo_total(this,
                 bordado,
                 quantidade
             )
 
-            Toast.makeText(
-                this,
-                "Pontos: ${bordado.pontos}," +
-                        " Cores: ${bordado.cores}," +
-                        " Qtde: $quantidade," +
-                        " Bastidor (bordado): ${bordado.bastidor}" +
-                        " Custo Calculado: $custo",
-                Toast.LENGTH_LONG
-            )
-                .show()
+            val custoFormatado = custo.formataParaBrasileiro()
+
+//            Toast.makeText(
+//                this,
+//                "Pontos: ${bordado.pontos}," +
+//                        " Cores: ${bordado.cores}," +
+//                        " Qtde: $quantidade," +
+//                        " Bastidor (bordado): ${bordado.bastidor}" +
+//                        " Custo Calculado: $custoFormatado",
+//                Toast.LENGTH_LONG
+//            )
+//                .show()
         }
     }
 
+    private fun calcula_custos_tempo_bordado(tempo_bordado: Double): BigDecimal {
+        val horas_trabalho_dia = 4.0 // Horas
+        val dias_trabalho_mes = 20 // dias
+        val horas_trabalho_mes = dias_trabalho_mes * horas_trabalho_dia
+
+        val salario_base = 1045.0 // Reais
+        val valor_hora_pessoa = salario_base / horas_trabalho_mes
+        val custo_mao_obra = (valor_hora_pessoa / 60) * tempo_bordado
+
+        val manutencao_anual = 300.0 // Reais
+        val luz = 40.0 // Reais
+        val agua = 0.0 // Reais
+        val aluguel = 0.0 // Reais
+        val telefone_internet = 50.0 // Reais
+        // X = 25 + 40 + 50 = 115
+        val custos_fixos_mensais = (manutencao_anual / 12) + luz +
+                agua + aluguel + telefone_internet
+
+        // X = ((115/80)/60) * (20 + 3 + 2) = ((1,4375)/60) * 25 = 0.024 * 25 = 0,60
+        val custos_fixos_por_bordado =
+            ((custos_fixos_mensais / horas_trabalho_mes) / 60) * tempo_bordado
+
+        return BigDecimal(custo_mao_obra + custos_fixos_por_bordado)
+    }
+
+    private fun calcula_tempo_bordado(bordado: Bordado): Double {
+        // Calculo de tempo de bordado
+        val velocidade_maquina = 500 // Pontos por minuto
+        val tempo_troca_cor = 1.0 // Minutos
+        val tempo_preparacao = 3.0 // Minutos
+
+        val tempo_bordado = (bordado.pontos / velocidade_maquina) +
+                (tempo_troca_cor * bordado.cores) + tempo_preparacao
+        return tempo_bordado
+    }
+
+    @SuppressLint("ShowToast")
     private fun calcula_custo_total(
+        context: Context,
         bordado: Bordado,
         quantidade: String
-    ): Double {
+    ): BigDecimal {
         val custo_linha_bordado = calcula_custo_linha_bordado(bordado)
         val custo_linha_bobina = calcula_custo_linha_bobina(bordado)
         val custo_entretela = calcula_custo_entretela(bordado)
+        val tempo_bordado = calcula_tempo_bordado(bordado)
+        val custos_tempo_bordado = calcula_custos_tempo_bordado(tempo_bordado)
+
         val custo = (
-                custo_linha_bordado + custo_linha_bobina + custo_entretela
-                ) * quantidade.toInt()
+                custo_linha_bordado +
+                        custo_linha_bobina +
+                        custo_entretela +
+                        custos_tempo_bordado
+                ) * quantidade.toBigDecimal()
+
+        Toast.makeText(context,
+                "Custos: ${custo_linha_bordado.formataParaBrasileiro()} + " +
+                "${custo_linha_bobina.formataParaBrasileiro()} + " +
+                "${custo_entretela.formataParaBrasileiro()} + " +
+                "${custos_tempo_bordado.formataParaBrasileiro()} = " +
+                        "${custo.formataParaBrasileiro()}",
+            Toast.LENGTH_LONG
+        ).show()
         return custo
     }
 
-    private fun calcula_custo_entretela(bordado: Bordado): Double {
-        val custo_metro = 12.0
+    private fun calcula_custo_entretela(bordado: Bordado): BigDecimal {
+        val custo_metro = BigDecimal("12.000000")
         val largura = 900 // milimetros
         val comprimento = 1000 // milimetros
 
@@ -81,30 +142,39 @@ class Inicial : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
         val quantidade_de_bastidores_por_area_de_entretela = area_entretela / area_bastidor
 
-        val custo_entretela = custo_metro / quantidade_de_bastidores_por_area_de_entretela
+        val custo_entretela = custo_metro / quantidade_de_bastidores_por_area_de_entretela.toBigDecimal()
 
         return custo_entretela
     }
 
-    private fun calcula_custo_linha_bobina(bordado: Bordado): Double {
-        val custo_cone_linha = 30.0
+    private fun calcula_custo_linha_bobina(bordado: Bordado): BigDecimal {
+        val custo_cone_linha = BigDecimal("30.000000")
         val quantidade_linha_por_cone = 15000
         val consumo_linha_por_1000_pontos = 2.5
 
-        val custo_metro_linha = custo_cone_linha / quantidade_linha_por_cone
+        val custo_metro_linha = custo_cone_linha / quantidade_linha_por_cone.toBigDecimal()
         val consumo_do_bordado = (bordado.pontos / 1000) * consumo_linha_por_1000_pontos
-        val custo_linha_bobina = custo_metro_linha * consumo_do_bordado
+        val custo_linha_bobina = custo_metro_linha * consumo_do_bordado.toBigDecimal()
         return custo_linha_bobina
     }
 
-    private fun calcula_custo_linha_bordado(bordado: Bordado): Double {
-        val custo_cone_linha: Double = 10.0
+    private fun calcula_custo_linha_bordado(bordado: Bordado): BigDecimal {
+        val custo_cone_linha = BigDecimal("10.000000")
         val quantidade_linha_por_cone: Int = 4000
         val consumo_linha_por_1000_pontos: Double = 6.5
 
-        val custo_metro_linha = custo_cone_linha / quantidade_linha_por_cone
+        val custo_metro_linha = custo_cone_linha.div(quantidade_linha_por_cone.toBigDecimal())
         val consumo_do_bordado = (bordado.pontos / 1000) * consumo_linha_por_1000_pontos
-        val custo_linha_bordado = custo_metro_linha * consumo_do_bordado
+        val custo_linha_bordado = custo_metro_linha.multiply(consumo_do_bordado.toBigDecimal())
+//        Toast.makeText(this,
+//            "$custo_cone_linha " +
+//                    "${quantidade_linha_por_cone.toBigDecimal()} " +
+//                    "$custo_metro_linha, " +
+//                    "$consumo_do_bordado, " +
+//                    "${custo_linha_bordado}, " +
+//                    "${custo_linha_bordado}",
+//            Toast.LENGTH_LONG).show()
+
         return custo_linha_bordado
     }
 
@@ -116,14 +186,15 @@ class Inicial : AppCompatActivity(), AdapterView.OnItemSelectedListener {
             bastidor_spinner.adapter = adapter
         }
 
-        bastidor_spinner.onItemSelectedListener = this
+//        bastidor_spinner.onItemSelectedListener = this
     }
 
-    override fun onNothingSelected(parent: AdapterView<*>?) {
-        TODO("Not yet implemented")
-    }
-
-    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        bastidor = parent!!.getItemAtPosition(position) as Bastidor
-    }
+//    override fun onNothingSelected(parent: AdapterView<*>?) {
+//        TODO("Not yet implemented")
+//    }
+//
+//    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+//        bastidor = parent!!.getItemAtPosition(position) as Bastidor
+//    }
 }
+
